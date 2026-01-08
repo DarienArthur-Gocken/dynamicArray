4438d0320dadd28d82eef05aa752ba5a72e97bc1
Darien Arthur-Gocken <233949874+DarienArthur-Gocken@users.noreply.github.com>
2026-01-08 15:01:52 -0800

    addstringmultiple & removestringtest

--- DynamicStringListTest.java
+++ DynamicStringListTest.java
@@ -10,4 +10,23 @@ public class DynamicStringListTest {
         test.add("Test");
         assertEquals(test.get(0), "Test");
     }
+
+    @Test
+    public void addStringMultiple() {
+        DynamicStringList test = new DynamicStringList();
+        test.add("Test");
+        test.add("Test2");
+        test.add("Test3");
+        assertEquals(test.get(1), "Test2");
+    }
+
+    @Test
+    public void removeStringTest() {
+        DynamicStringList test = new DynamicStringList();
+        test.add("Test");
+        test.add("Test2");
+        test.add("Test3");
+        test.remove(1);
+        assertEquals(test.get(1), "Test3");
+    }
 }
