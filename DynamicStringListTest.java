6c9b4e4d93f4be3ac3aaaf78503b039c4f4b027e
anthonykravchishin <tonykravc53@gmail.com>
2026-01-08 15:00:31 -0800

    Testing

--- DynamicStringListTest.java
+++ DynamicStringListTest.java
@@ -1,3 +1,13 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class DynamicStringListTest {
-    
+
+    @Test
+    public void addStringAndGet() {
+        DynamicStringList test = new DynamicStringList();
+        test.add("Test");
+        assertEquals(test.get(0), "Test");
+    }
 }
